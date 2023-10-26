18e1d57c0af3bfbcf1669c8e08ecc56245afa998
Yasmin de França <127551306+yasmin-de-franca@users.noreply.github.com>
2023-10-26 11:16:31 -0300

    Updated Curso-Python

--- exercicios/ex003.py
+++ exercicios/ex003.py
@@ -1,4 +1,6 @@
 n1 = int(input('Digite um número:'))
 n2 = int(input('Digite outro número:'))
-s = n1+n2
-print('A soma vale {}' .format(s))
+s = n1 + n2
+# print('A soma entre', n1 'e', n2 ' vale', s)
+# print('A soma vale {}' .format(s))
+print('A soma entre {} + {} vale {}' .format(n1, n2, s))
