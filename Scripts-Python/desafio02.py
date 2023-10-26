cfc793f1d61f6a926289c171f3b246336928c1a9
Yasmin de França <127551306+yasmin-de-franca@users.noreply.github.com>
2023-10-26 11:00:35 -0300

    Updated TXT's, Scripts and exercicios

--- Scripts-Python/desafio02.py
+++ Scripts-Python/desafio02.py
@@ -1,1 +1,8 @@
-dia = input('Em que dia você nasceu')
+nome = input('Qual é o seu nome?')
+print('Olá' + nome +'!', 'Prazer em te conhecer! Vamos precisar de mais alguns dados ok?')
+
+dia = input('Em que dia você nasceu?')
+mês = input('Em que mês você nasceu?')
+ano = input('E em qual ano você nasceu?')
+
+print('Você nasceu no dia' + dia + ', mês' + mês + ' e no ano de' + ano +'. ''Correto?')
--- Scripts-Python/teste01.py
+++ Scripts-Python/teste01.py
@@ -2,4 +2,4 @@ nome = input('Qual é o seu nome?')
 idade = input('Qual é a sua idade?')
 peso = input('Quanto você pesa?')
 
-print(nome, idade, peso)
+print(nome, idade, peso) 
